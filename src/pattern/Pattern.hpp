aed5797f9985dde0ed6a7951d96356e567181b1f
EC2 Default User <ec2-user@ip-172-31-63-13.ec2.internal>
2021-12-23 19:42:47 +0000

    Added documentation

--- src/pattern/Pattern.hpp
+++ src/pattern/Pattern.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file Pattern.hpp
+ * @author Jesse McKinzie (Jesse.McKinzie@axleinfo.com)
+ * @brief Parent class of FilePattern and ExternalFilePattern
+ * @version 0.1
+ * @date 2021-12-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #pragma once
 #include <string>
 #include <iostream>
@@ -11,9 +22,9 @@ class Pattern {
     
     protected:
         std::regex regexExpression; // Regex expression
-        std::string filePattern;
-        std::string regexFilePattern;
-        std::vector<std::string> variables;
+        std::string filePattern; // Pattern to match files to
+        std::string regexFilePattern; // Pattern with capture groups
+        std::vector<std::string> variables; // Store the names of variables from the pattern
 
 
     public:
@@ -28,17 +39,65 @@ class Pattern {
          */
         void filePatternToRegex();
 
-        Tuple getVariableMapMultDir(const std::string& filePattern, const std::smatch& sm);
+        /**
+         * @brief Get the mapping of variables to values for a matching file. Used with a recursive directory iterator. 
+         * 
+         * Uses the regex version of the pattern from filePatternToRegex() to extract capture groups from
+         * a basename. Returns a tuple of variable matched to capture group and the file path if no file with 
+         * the same basename has already been matched and an empty tuple otherwise. In the later case, the 
+         * basename is appending to the second member of the existing tuple.
+         * 
+         * @param filePattern filePath that matches the pattern
+         * @param sm captured groups
+         * @return Tuple A tuple with the mapping in first and the file path in second. An empty tuple is returned
+         * if the basename of filePath has already been matched
+         */
+        Tuple getVariableMapMultDir(const std::string& filePath, const std::smatch& sm);
 
+
+         /**
+         * @brief Get the mapping of variables to values for a matching file. Used with a directory iterator.
+         * 
+         * Uses the regex version of the pattern from filePatternToRegex() to extract capture groups from
+         * a basename. Returns a tuple of variable matched to capture group.
+         * 
+         * @param filePattern filePath that matches the pattern
+         * @param sm captured groups
+         * @return Tuple A tuple with the mapping in first and the file path in second 
+         */
         Tuple getVariableMap(const std::string& filePath, const std::smatch& sm);
 
+        /**
+         * @brief Get the pattern to match files to.
+         * 
+         * @return std::string The pattern that files are matched to
+         */
         std::string getPattern();
 
-        void setPattern(const std::string&);
+        /**
+         * @brief Set the pattern.
+         * 
+         * @param pattern New pattern
+         */
+        void setPattern(const std::string& pattern);
 
+        /**
+         * @brief Get the pattern with regex capture groups
+         * 
+         * @return std::string The pattern with regex capture groups
+         */
         std::string getRegexPattern();
         
+        /**
+         * @brief Get the variable names
+         * 
+         * @return std::vector<std::string> Vector of variable names
+         */
         std::vector<std::string> getVariables();
 
+        /**
+         * @brief Prints the variables to the console.
+         * 
+         */
         void printVariables();
 };
