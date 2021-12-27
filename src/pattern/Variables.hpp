c06715ac45bd5b68f2380886e9b8e9328e985a1a
EC2 Default User <ec2-user@ip-172-31-63-13.ec2.internal>
2021-12-27 17:42:46 +0000

    Update

--- src/pattern/Variables.hpp
+++ src/pattern/Variables.hpp
@@ -58,7 +58,7 @@ namespace s {
          * @param delimiter Character to split string on
          * @return std::vector<std::string> Vector of split strings
          */
-        inline std::vector<std::string> split (std::string& s, const std::string& delimiter) {
+        inline std::vector<std::string> split(std::string& s, const std::string& delimiter) {
                 size_t pos_start = 0, pos_end, delim_len = delimiter.length();
                 std::string token;
                 std::vector<std::string> res;
