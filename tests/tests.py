d95914c74a118e71f919e6867816ce2dd83c4bd8
EC2 Default User <ec2-user@ip-172-31-63-13.ec2.internal>
2022-01-12 21:13:49 +0000

    Added tests for ExternalStringPattern

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,6 @@ import unittest
 import generate_data
 from pattern import FilePattern as fp
 from pattern import StringPattern as sp
-from pattern import ExternalFilePattern as efp
 import filepattern
 import os
 import pprint
@@ -14,11 +13,12 @@ class TestFilePattern(unittest.TestCase):
 
     old_pattern = 'img_r{rrr}_c{ccc}.tif'
 
-    patterns = ['img_r{r:ddd}_c{c:ddd}.tif', 'img_r{r:d+}_c{c:d+}.tif']
+    patterns = ['img_r{r:ddd}_c{c:ddd}.tif', 'img_r{r:d+}_c{c:d+}.tif', old_pattern]
 
     MAX_NUM = 9
 
     def test_file_pattern(self):
+        print("testing normal")
 
         for pattern in self.patterns:
             old_files = filepattern.FilePattern(self.path, self.old_pattern)
@@ -132,7 +132,7 @@ class TestFilePattern(unittest.TestCase):
 
         for pattern in self.patterns:
             old_files = filepattern.FilePattern(old_path, self.old_pattern)
-            files = fp.FilePattern(path, pattern, True)
+            files = fp.FilePattern(path, pattern, recursive=True)
 
             old_result = []
             result = []
@@ -170,12 +170,12 @@ class TestStringPattern(unittest.TestCase):
 
     old_pattern = 'img_r{rrr}_c{ccc}.tif'
 
-    patterns = ['img_r{r:ddd}_c{c:ddd}.tif', 'img_r{r:d+}_c{c:d+}.tif']
+    patterns = ['img_r{r:ddd}_c{c:ddd}.tif', 'img_r{r:d+}_c{c:d+}.tif', old_pattern]
 
     MAX_NUM = 9
 
     def test_file_pattern(self):
-
+        print("testing string")
         for pattern in self.patterns:
             old_files = filepattern.FilePattern(self.path, self.old_pattern)
             files = sp.StringPattern(self.filepath, pattern)
@@ -292,16 +292,16 @@ class TestExternalFilePattern(unittest.TestCase):
 
     patterns = ['img_r{r:ddd}_c{c:ddd}.tif']#, 'img_r{r:d+}_c{c:d+}.tif']
 
-    block_sizes = ['300 KB']#, '300 KB'] # first value is to process in multiple blocks and second is to proces in one block
+    block_sizes = ['300 MB']#, '300 KB'] # first value is to process in multiple blocks and second is to proces in one block
 
     MAX_NUM = 1
 
     def test_file_pattern(self):
-
+        print("testing external")
         for pattern in self.patterns:
             for block_size in self.block_sizes:
                 old_files = filepattern.FilePattern(self.path, self.old_pattern)
-                files = efp.FilePattern(self.path, pattern, block_size)
+                files = fp.FilePattern(self.path, pattern, block_size=block_size)
 
                 old_result = []
                 result = []
@@ -309,13 +309,8 @@ class TestExternalFilePattern(unittest.TestCase):
                 for file in old_files():
                     old_result.append(file)
 
-                while(True):    
-                    for file in files():
-                        result.append(file)
-
-                    #files.next()
-                    if(len(files) == 0):
-                        break
+                for file in files():
+                    result.append(file)
 
                 self.assertEqual(len(old_result), len(result)) 
 
@@ -326,29 +321,22 @@ class TestExternalFilePattern(unittest.TestCase):
 
 
     def test_get_matching(self):
+        print("testing matching in external")
         for pattern in self.patterns:
             nums = []
             for block_size in self.block_sizes:
                 for i in range(0, self.MAX_NUM):
 
                     old_files = filepattern.FilePattern(self.path, self.old_pattern)
-                    files = efp.FilePattern(self.path, pattern, block_size)
+                    files = fp.FilePattern(self.path, pattern, block_size=block_size)
 
                     nums.append(i)
 
                     old_result = old_files.get_matching(R=[i])
 
-                    files.get_matching(r=[i])
-
                     result = []
-                    while(True):
-                        matching = files.get_matching_block()
-
-                        for match in matching:
-                            result.append(match)
-
-                        if(len(matching) == 0):
-                            break
+                    for match in files.get_matching(r=[i]):
+                        result.append(match)
                     
                     self.assertEqual(len(old_result), len(result)) 
 
@@ -358,17 +346,9 @@ class TestExternalFilePattern(unittest.TestCase):
                         self.assertEqual(str(old_result[i]['file']), result[i][1][0])
 
                     old_result = old_files.get_matching(C=[i])
-                    files.get_matching(c=[i])
-                    
                     result = []
-                    while(True):
-                        matching = files.get_matching_block()
-
-                        for match in matching:
-                            result.append(match)
-
-                        if(len(matching) == 0):
-                            break
+                    for match in files.get_matching(c=[i]):
+                        result.append(match)
 
                     self.assertEqual(len(old_result), len(result)) 
 
@@ -378,17 +358,11 @@ class TestExternalFilePattern(unittest.TestCase):
                         self.assertEqual(str(old_result[i]['file']), result[i][1][0])
 
                     old_result = old_files.get_matching(R=nums)
-                    files.get_matching(r=nums)
+                    #files.get_matching(r=nums)
                     
                     result = []
-                    while(True):
-                        matching = files.get_matching_block()
-
-                        for match in matching:
-                            result.append(match)
-
-                        if(len(matching) == 0):
-                            break
+                    for match in files.get_matching(r=nums):
+                        result.append(match)
 
                     self.assertEqual(len(old_result), len(result)) 
 
@@ -398,17 +372,11 @@ class TestExternalFilePattern(unittest.TestCase):
                         self.assertEqual(str(old_result[i]['file']), result[i][1][0])
 
                     old_result = old_files.get_matching(C=nums)
-                    files.get_matching(c=nums)
+                    #files.get_matching(c=nums)
                     
                     result = []
-                    while(True):
-                        matching = files.get_matching_block()
-
-                        for match in matching:
-                            result.append(match)
-
-                        if(len(matching) == 0):
-                            break
+                    for match in files.get_matching(c=nums):
+                        result.append(match)
 
                     self.assertEqual(len(old_result), len(result)) 
 
@@ -422,7 +390,7 @@ class TestExternalFilePattern(unittest.TestCase):
         for pattern in self.patterns:
             for block_size in self.block_sizes:
                 old_files = filepattern.FilePattern(self.path, self.old_pattern)
-                files = efp.FilePattern(self.path, pattern, block_size)
+                files = fp.FilePattern(self.path, pattern, block_size=block_size)
 
                 old_result = []
                 result = []
@@ -432,17 +400,9 @@ class TestExternalFilePattern(unittest.TestCase):
                     
                     old_result.append(file)
 
-                while(True):    
-                    temp = []
-                    for file in files(group_by="r"):
-                        temp.append(file)
-                    
-                    #files.next()
-                    if(len(files) == 0):
-                        break
 
-                    result.append(temp)
-
+                for file in files(group_by="r"):
+                    result.append(file)
                         
 
                 self.assertEqual(len(old_result), len(result)) 
@@ -461,22 +421,164 @@ class TestExternalFilePattern(unittest.TestCase):
                 for file in old_files(group_by="r"):
                     old_result.append(file)
 
-                while(True):    
-                    temp = []
-                    for file in files(group_by="c"):
-                        temp.append(file)
-                    
-                    #files.next()
-                    if(len(files) == 0):
-                        break
 
-                    result.append(temp)
+                for file in files(group_by="c"):
+                    result.append(file)                    
+
+                self.assertEqual(len(old_result), len(result)) 
 
                 for i in range(len(old_result)):
-                    pprint.pprint(old_result[i])
-                    pprint.pprint(result[i])
-                    print()
+                    for j in range(len(old_result[i])):
+                        self.assertEqual(old_result[i][j]["r"], result[i][j][0]["r"]) 
+                        self.assertEqual(old_result[i][j]["c"], result[i][j][0]["c"])
+                        self.assertEqual(str(old_result[i][j]['file']), result[i][j][1][0])
+
+
+
+class TestExternalStringPattern(unittest.TestCase):
+    root_directory = os.path.dirname(os.path.realpath(__file__))
+
+    path = root_directory + '/test_data/data100'
+    string_path = root_directory + '/test_data/data100.txt'
+
+    old_pattern = 'img_r{rrr}_c{ccc}.tif'
+
+    patterns = ['img_r{r:ddd}_c{c:ddd}.tif']#, 'img_r{r:d+}_c{c:d+}.tif']
+
+    block_sizes = ['300 MB']#, '300 KB'] # first value is to process in multiple blocks and second is to proces in one block
+
+    MAX_NUM = 1
+
+    def test_file_pattern(self):
+        print("testing external")
+        for pattern in self.patterns:
+            for block_size in self.block_sizes:
+                old_files = filepattern.FilePattern(self.path, self.old_pattern)
+                files = sp.StringPattern(self.string_path, pattern, block_size=block_size)
+
+                old_result = []
+                result = []
+
+                for file in old_files():
+                    old_result.append(file)
+
+                for file in files():
+                    result.append(file)
+
+                self.assertEqual(len(old_result), len(result)) 
+
+                for i in range(len(old_result)):
+                    self.assertEqual(old_result[i][0]["r"], result[i][0]["r"]) 
+                    self.assertEqual(old_result[i][0]["c"], result[i][0]["c"])
+                    self.assertEqual(str(os.path.basename(old_result[i][0]['file'])), result[i][1][0])
+
+
+    def test_get_matching(self):
+        print("testing matching in external")
+        for pattern in self.patterns:
+            nums = []
+            for block_size in self.block_sizes:
+                for i in range(0, self.MAX_NUM):
+
+                    old_files = filepattern.FilePattern(self.path, self.old_pattern)
+                    files = sp.StringPattern(self.string_path, pattern, block_size=block_size)
+
+                    nums.append(i)
+
+                    old_result = old_files.get_matching(R=[i])
+
+                    result = []
+                    for match in files.get_matching(r=[i]):
+                        pprint.pprint(result)
+                        pprint.pprint(result)
+                        result.append(match)
                     
+                    self.assertEqual(len(old_result), len(result)) 
+
+                    for i in range(len(old_result)):                
+                        self.assertEqual(old_result[i]["r"], result[i][0]["r"]) 
+                        self.assertEqual(old_result[i]["c"], result[i][0]["c"])
+                        self.assertEqual(str(os.path.basename(old_result[i]['file'])), result[i][1][0])
+
+                    old_result = old_files.get_matching(C=[i])
+                    result = []
+                    for match in files.get_matching(c=[i]):
+                        result.append(match)
+
+                    self.assertEqual(len(old_result), len(result)) 
+
+                    for i in range(len(old_result)):                
+                        self.assertEqual(old_result[i]["r"], result[i][0]["r"]) 
+                        self.assertEqual(old_result[i]["c"], result[i][0]["c"])
+                        self.assertEqual(str(os.path.basename(old_result[i]['file'])), result[i][1][0])
+
+                    old_result = old_files.get_matching(R=nums)
+                    #files.get_matching(r=nums)
+                    
+                    result = []
+                    for match in files.get_matching(r=nums):
+                        result.append(match)
+
+                    self.assertEqual(len(old_result), len(result)) 
+
+                    for i in range(len(old_result)): 
+                        self.assertEqual(old_result[i]["r"], result[i][0]["r"]) 
+                        self.assertEqual(old_result[i]["c"], result[i][0]["c"])
+                        self.assertEqual(str(os.path.basename(old_result[i]['file'])), result[i][1][0])
+
+                    old_result = old_files.get_matching(C=nums)
+                    #files.get_matching(c=nums)
+                    
+                    result = []
+                    for match in files.get_matching(c=nums):
+                        result.append(match)
+
+                    self.assertEqual(len(old_result), len(result)) 
+
+                    for i in range(len(old_result)): 
+                        self.assertEqual(old_result[i]["r"], result[i][0]["r"]) 
+                        self.assertEqual(old_result[i]["c"], result[i][0]["c"])
+                        self.assertEqual(str(os.path.basename(old_result[i]['file'])), result[i][1][0])
+
+
+    def test_group_by(self):
+        for pattern in self.patterns:
+            for block_size in self.block_sizes:
+                old_files = filepattern.FilePattern(self.path, self.old_pattern)
+                files = sp.StringPattern(self.string_path, pattern, block_size=block_size)
+
+                old_result = []
+                result = []
+
+                # group by "c" instead of "r" since we changed how group by works
+                for file in old_files(group_by="c"):
+                    
+                    old_result.append(file)
+
+
+                for file in files(group_by="r"):
+                    result.append(file)
+                        
+
+                self.assertEqual(len(old_result), len(result)) 
+                for i in range(len(old_result)):
+                   self.assertEqual(len(old_result[i]), len(result[i])) 
+                
+                for i in range(len(old_result)):
+                    for j in range(len(old_result[i])):
+                        self.assertEqual(old_result[i][j]["r"], result[i][j][0]["r"]) 
+                        self.assertEqual(old_result[i][j]["c"], result[i][j][0]["c"])
+                        self.assertEqual(str(old_result[i][j]['file']), result[i][j][1][0])
+
+                result = []
+                old_result = []
+
+                for file in old_files(group_by="r"):
+                    old_result.append(file)
+
+
+                for file in files(group_by="c"):
+                    result.append(file)                    
 
                 self.assertEqual(len(old_result), len(result)) 
 
