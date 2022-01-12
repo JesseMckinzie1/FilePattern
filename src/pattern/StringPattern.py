ff21efbacf347fae71a8e4b262abdd128f75202e
EC2 Default User <ec2-user@ip-172-31-63-13.ec2.internal>
2022-01-12 21:12:56 +0000

    Created parent class to create StringPattern or ExternalStringPattern object based on arguments

--- src/pattern/StringPattern.py
+++ src/pattern/StringPattern.py
@@ -1,34 +1,11 @@
-from numpy import string_
-from . import backend
-import logging
+from . import ExternalStringPattern, InternalStringPattern
 
 class StringPattern:
-
-    def __init__(self, filePath: str, pattern: str):
-        """
-        Constructor of the FilePattern class.
-
-        Creates a new file pattern object given a path to a 
-        directory of files and a pattern.
-
-        :param str path: path to directory 
-        :param str pattern: file pattern
-        :param bool debug: debug mode on or off. When true, debug statements are printed to the consol
-
-        Valid patterns are d, c, and +, where d is a digit, c is
-        a character, and + means an arbitrary number of the pattern it 
-        acts on. 
-
-        Example: The pattern files_x{row:ddd}_y{col:ddd}_{channel: c+}.ome.tif
-        would match files with 3 digits after x, 3 digits after y, and then an 
-        arbitrary number of characters. 
-        """
-
-        try:
-            self._file_pattern = backend.StringPattern(filePath, pattern)
-        except RuntimeError as e:
-            #error = "No directory found. Invalid path " + filePath
-            print(e)
+    def __init__(self, path: str, pattern: str, block_size: str=""): 
+        if(block_size == ""): 
+            self._file_pattern = InternalStringPattern.InternalStringPattern(path, pattern)
+        else: 
+            self._file_pattern = ExternalStringPattern.ExternalStringPattern(path, pattern, block_size=block_size)
 
     def get_matching(self, **kwargs) -> list:
         """
@@ -43,30 +20,17 @@ class StringPattern:
             for key, value in kwargs.items():
                 mapping.append((key, value))
 
-            return self._file_pattern.getMatching(mapping)
+            return self._file_pattern.get_matching(mapping)
         except ValueError as e:
             print(e)
             
+    def set_pattern(self, pattern: str) -> None:
+        """
+        Sets the file pattern parameter.
 
-    def print_valid_files(self) -> None:
+        :param str pattern: filepattern
         """
-        Prints out files that match the file pattern to the console.
-        """
-        
-        self._file_pattern.printValidFiles()
-    
-    def match_files(self) -> None:
-        """
-        Comapres files to file pattern and stores file names that match
-        the file pattern.
-
-        :param bool cut_path: Cuts the path off of the filename, leaving only the filename (otional, defaults to true)
-        e.g. /home/usr/file.txt -> file.txt when true
-        """
-        try: 
-            self._file_pattern.matchFiles()
-        except ValueError as e: 
-            print(e)
+        self._file_pattern.setPattern(pattern)
 
     def group_by(self, group: str) -> None:
         """
@@ -74,14 +38,26 @@ class StringPattern:
 
         :param str group: variable to group by
         """
-        self._file_pattern.groupBy(group)
+        self._file_pattern.group_by(group)
+
+    def get_occurences(self, **kwargs):
+        mapping = []
+        for key, value in kwargs.items():
+            mapping.append((key, value))
+
+        return self._file_pattern.get_occurences(mapping)
+    
+    def get_unique_values(self, *args) -> list:
+        vec = []
+        for str in args:
+            vec.append(str)
+
+        return self._file_pattern.get_unique_values(vec)
 
     
     def __call__(self, group_by=None):
-        if(group_by is not None):
-            self._file_pattern.groupBy(group_by)
         
-        return self
+        return self._file_pattern.__call__(group_by)
 
     def __iter__(self):
         """
