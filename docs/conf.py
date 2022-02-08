2d81b3336600348ebc2db81d34dc294dacdb8b47
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2022-02-08 15:59:38 -0500

    Changed reqs

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,10 @@ extensions = [
     'autodocsumm'
 ]
 
+install_requires = [
+    'gnu>=8.0'
+]
+
 napoleon_use_param = True
 
 # Add any paths that contain templates here, relative to this directory.
